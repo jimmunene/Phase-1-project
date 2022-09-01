7ce7b02a5105e89f1fcc885aa3fa4ab40f3eb54d
jimbrian <ui.ux.jimbrian@gmail.com>
2022-09-01 10:47:54 +0300

    Used Fetch from Developer quotes API

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ twitterBtn = document.querySelector(".twitter"),
 heartBtn = document.querySelector(".heart")
 synth = speechSynthesis;
 
+
+function randomQuote(){
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading Quote";
+    fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
+    .then(response => response.json())
+    .then(result => {
+        quoteText.innerText = result.en;
+        authorName.innerText = result.author;
+        quoteBtn.classList.remove("loading");
+        quoteBtn.innerText = "Get New Quote";
+    });
+}
