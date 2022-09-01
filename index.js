19e184017eb67076c94c227d2c847ca9a5c47478
jimbrian <ui.ux.jimbrian@gmail.com>
2022-09-01 10:46:23 +0300

    Grabbed all buttons

---
+++ index.js
@@ -0,0 +1,9 @@
+const quoteText = document.querySelector(".quote"),
+quoteBtn = document.querySelector("button"),
+authorName = document.querySelector(".name"),
+speechBtn = document.querySelector(".speech"),
+copyBtn = document.querySelector(".copy"),
+twitterBtn = document.querySelector(".twitter"),
+heartBtn = document.querySelector(".heart")
+synth = speechSynthesis;
+
